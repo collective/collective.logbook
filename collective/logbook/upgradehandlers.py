85560812598d12d4bff338536f730acde8fe26ec
Rafael Oliveira <rafaelbco@gmail.com>
2017-02-22 16:34:32 -0300

    Added upgrade step which creates the registry records
    Fix #11

--- collective/logbook/upgradehandlers.py
+++ collective/logbook/upgradehandlers.py
@@ -11,6 +11,37 @@ from collective.logbook.interfaces import ILogBookStorage
 from collective.logbook.config import STORAGE_KEY
 from collective.logbook.config import INDEX_KEY
 
+from plone import api
+
+
+PACKAGE_NAME = 'collective.logbook'
+
+
+def generic_setup_profile_id(package_name, profile='default'):
+    return 'profile-{package_name}:{profile}'.format(
+        package_name=package_name,
+        profile=profile
+    )
+
+
+def run_import_step(profile_id, import_step):
+    u"""Run an import step.
+
+    Arguments:
+    profile_id -- Profile ID. Eg.: "profile-my.package:default"
+    import_step -- Import step IP. Eg.: "actions".
+    """
+    setup = api.portal.get_tool('portal_setup')
+    setup.runImportStepFromProfile(profile_id, import_step)
+
+
+def run_registry_import_step(context):
+    profile_id = generic_setup_profile_id(
+        package_name=PACKAGE_NAME,
+        profile='default'
+    )
+    run_import_step(profile_id=profile_id, import_step='plone.app.registry')
+
 
 def migrate_storage(context):
     portal_url = getToolByName(context, 'portal_url')
