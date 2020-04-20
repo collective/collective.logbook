ec52411b6d2065241f4bfdb7ea70471d9c8e67c8
Rafael Oliveira <rafaelbco@gmail.com>
2020-04-20 10:27:42 -0300

    Fix #19: Cannot handle traceback when error occurs when accessing a method of a view (#20)
    * Get the right error context when accessing a view's method directly (#19)
    
    * Improve tests for cases where error context is acquisition wrapped
    
    Make acquisition more explicit and acquire the error_log object the same way Zope does.

--- collective/logbook/tests/test_events.py
+++ collective/logbook/tests/test_events.py
@@ -4,6 +4,9 @@ from plone import api
 import sys
 import unittest
 
+from Acquisition import ImplicitAcquisitionWrapper
+from Acquisition import aq_acquire
+
 from collective.logbook.interfaces import ILogBookStorage
 from collective.logbook.tests.base import LogBookFunctionalTestCase
 
@@ -25,16 +28,32 @@ class TestEvents(LogBookFunctionalTestCase):
         self.assertEqual(len(self._get_errors_by_prefix(prefix)), 2)
 
     def test_error_is_logged_with_acquisition_wrapped_context(self):
-        # Note: this is a regression test for a bug that occurs when the context of the error
-        # is inside an acquisition wrapper.
+        # Note: this is a regression test for issue #11: "Error in unknown conditions: TypeError:
+        # Can't pickle objects in acquisition wrappers"
 
         folder = api.content.create(container=self.portal, type='Folder', title='Folder 0')
+        aq_wrapped_folder = ImplicitAcquisitionWrapper(folder, self.portal)
 
         prefix = 'test1_'
         self.assertEqual(len(self._get_errors_by_prefix(prefix)), 0)
         self._simulate_error(prefix + '0')
         self.assertEqual(len(self._get_errors_by_prefix(prefix)), 1)
-        self._simulate_error(prefix + '1', context=folder)
+        self._simulate_error(prefix + '1', context=aq_wrapped_folder)
+        self.assertEqual(len(self._get_errors_by_prefix(prefix)), 2)
+
+    def test_error_is_logged_when_acessing_view_method_directly(self):
+        # Note: this is a regression test for issue #19: "Cannot handle traceback when error occurs
+        # when accessing a method of a view".
+
+        view = self.portal.unrestrictedTraverse('@@plone_portal_state')
+        aq_wrapped_view = ImplicitAcquisitionWrapper(view, self.portal)
+        prefix = 'test1_'
+        self.assertEqual(len(self._get_errors_by_prefix(prefix)), 0)
+        self._simulate_error(prefix + '0')
+        self.assertEqual(len(self._get_errors_by_prefix(prefix)), 1)
+
+        # Simulate the failure mode by setting the error context as the view itself.
+        self._simulate_error(prefix + '1', context=aq_wrapped_view)
         self.assertEqual(len(self._get_errors_by_prefix(prefix)), 2)
 
     def _simulate_error(self, msg='Dummy error', context=None):
@@ -42,7 +61,9 @@ class TestEvents(LogBookFunctionalTestCase):
             raise RuntimeError(msg)
         except RuntimeError:
             context = context if (context is not None) else self.portal
-            error_log = context.unrestrictedTraverse('error_log')
+
+            # Acquire the error_log object the same way Zope does. See module: Zope2.App.startup
+            error_log = aq_acquire(context, '__error_log__', containment=1)
             error_log.raising(sys.exc_info())
 
     def _get_errors_by_prefix(self, prefix):
