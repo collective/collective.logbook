007bd2f5e2655a470fe34d90b97c240437a946e6
Ramon Bartl <rb@ridingbytes.com>
2020-04-22 13:04:48 +0200

    Fix tests

--- collective/logbook/__init__.py
+++ collective/logbook/__init__.py
@@ -2,9 +2,18 @@
 
 from zope.i18nmessageid import MessageFactory
 
+from collective.logbook import monkey
+
 logbookMessageFactory = MessageFactory('collective.logbook')
 
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product.
     """
+    # Note: Component Registry is not ready at this stage, so we can not query
+    #       the Plone registry to see if logbook logging is enabled or not.
+    #
+    #       => Install the monkeypatch always. The patched raising event will
+    #          check later the Plone registry and will uninstall the monkey
+    #          patch if logging is disabled (see monkey.py).
+    monkey.install_monkey()
